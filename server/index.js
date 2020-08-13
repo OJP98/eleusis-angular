43aaf6bb8e14ecc2be343b51fbc530025104b3e2
josepablito16 <josepablocif16@gmail.com>
2020-08-13 11:11:21 -0600

    Se agrega deck a la estructura de datos salas luego de repartir las cartas a los jugadores

--- server/index.js
+++ server/index.js
@@ -13,20 +13,16 @@ isValid = esto nos dice si la carta sigue la regla o no
 function Card(value, symbol) {
   this.symbol = symbol;
   if (Number(value) >= 11) {
-      if (Number(value) == 11) {
-          this.character = "J";
-      }
-      else if (Number(value) == 12) {
-          this.character = "Q";
-      }
-      else if (Number(value) == 13) {
-          this.character = "K";
-      }
-  }
-  else if(Number(value) == 1){
-      this.character = "A";
-  }
-  else{
+    if (Number(value) == 11) {
+      this.character = 'J';
+    } else if (Number(value) == 12) {
+      this.character = 'Q';
+    } else if (Number(value) == 13) {
+      this.character = 'K';
+    }
+  } else if (Number(value) == 1) {
+    this.character = 'A';
+  } else {
     this.character = value;
   }
   this.value = Number(value);
@@ -59,7 +55,6 @@ Card.prototype.valueAndSymbol = function () {
 /*
 Termina todo lo relacionado con la clase carta
 */
-
 
 var salas = {
   /*
@@ -239,33 +234,47 @@ function generar_deck() {
   Club = C
   Diamonds = D
   */
-  var symbol = new Array("spades", "hearts", "clubs", "diamonds");
-  var numbers = new Array("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13");
+  var symbol = new Array('spades', 'hearts', 'clubs', 'diamonds');
+  var numbers = new Array(
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    '11',
+    '12',
+    '13'
+  );
   for (var i = 0; i < 2; i++) {
-      for (var j in symbol) {
-          for (var k in numbers) {
-              var new_card = new Card(numbers[k], symbol[j]);
-              all_Cards.push(new_card);
-          }
+    for (var j in symbol) {
+      for (var k in numbers) {
+        var new_card = new Card(numbers[k], symbol[j]);
+        all_Cards.push(new_card);
       }
+    }
   }
   return all_Cards;
 }
 
 //
 function RepartirCartas(sala, God) {
-  var deck = generar_deck()
+  var deck = generar_deck();
   salas[sala].Sockets.forEach(function each(clientLoop) {
     // cada iteracion es un cliente menos el Dios
     if (clientLoop !== God) {
       // Array para guardar las 12 cartas
-      var player_deck = new Array;
+      var player_deck = new Array();
       //Se entra a un for, donde se repartiran 12 cartas
       for (var j = 0; j < 12; j++) {
-        var index = Math.floor(Math.random() * (deck.length));
+        var index = Math.floor(Math.random() * deck.length);
         player_deck.push(deck[index]);
         if (index > -1) {
-            deck.splice(index, 1);
+          deck.splice(index, 1);
         }
       }
       clientLoop.send(
@@ -276,6 +285,7 @@ function RepartirCartas(sala, God) {
       );
     }
   });
+  salas[sala].Deck = deck;
 }
 
 function IniciarJuego(request, client) {
